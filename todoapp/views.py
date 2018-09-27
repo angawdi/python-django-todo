0bf70d9e886c2f4cbaea34b8a7399c4a84ccfa85
Sky <anvandp18@gmail.com>
2018-09-27 14:38:30 -0700

    fix bug, now we could add the todo list now. but cannot delete or mark done yet

--- todoapp/views.py
+++ todoapp/views.py
@@ -6,10 +6,12 @@ from .models import Todo
 
 # Main routes
 def index(request):
+    todos = Todo.objects.all().order_by('text')
+    users = User.objects.all()
+    
     if request.method == "GET":
         # Display all the todos:
-        todos = Todo.objects.all().order_by('text')
-        users = User.objects.all()
+       
         return render(request, 
             'todoapp/index.html',
             {
@@ -17,16 +19,34 @@ def index(request):
                 'users': users
             })
     elif request.method == "POST":
+        print("POST")
         # Add a new todo
-        return HttpResponse('index POST')
+        try: 
+            user_id = request.POST['userid']
+        except:
+            return render(request,
+                'todoapp/index.html',
+                {
+                    'error': 'You must select a user',
+                    'todos': todos,
+                    'users': users
+                })
+        else:
+            new_todo = Todo()
+            new_todo.text = request.POST['text']
+            new_todo.user = User.objects.get(pk=user_id)
+            new_todo.save()
+            return redirect('index')
+
+    return HttpResponse('index POST')
 
 def delete(request, todo_id):
     # Delete a todo
     return HttpResponse("Delete this")
 
-def done():
+def done(request, todo_id):
     # will mark a todo as complete
-    return HttpResponse('done')
+    return HttpResponse('Mark done')
 
 # Auth-related routes
 def signup(request):
