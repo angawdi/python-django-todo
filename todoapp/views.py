4f90f308e0b2ffe316f1c3d4ab65316fa1c7d27a
Sky <anvandp18@gmail.com>
2018-09-27 13:39:39 -0700

    edit something

--- todoapp/views.py
+++ todoapp/views.py
@@ -6,17 +6,18 @@ from django.http import HttpResponse
 # Main routes
 def index(request):
     if request.method == "GET":
+        # Display all the todos:
         return HttpResponse('index GET')
     elif request.method == "POST":
+        # Add a new todo
         return HttpResponse('index POST')
 
 def delete(request, todo_id):
+    # Delete a todo
     return HttpResponse("Delete this")
 
-def delete(request, todo_id):
-    return HttpResponse("Mark done")
-
 def done():
+    # will mark a todo as complete
     return HttpResponse('done')
 
 # Auth-related routes
